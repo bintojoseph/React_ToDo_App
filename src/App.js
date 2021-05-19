737a4e40a73569ff77e450797bbb0600b1b01792
Binto Joseph <bintojoseph543@gmail.com>
2021-05-19 14:11:08 +0530

    add button modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+        <i onClick={()=>{
+          setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])
+          setToDo('')
+        }} 
+        className="fas fa-plus"></i>
       </div>
       <div className="todos">
         {
